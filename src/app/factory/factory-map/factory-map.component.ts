1ca0153c2ca979800f246bef0de24d0bb1a4be57
zpy <a@zpyyf.com>
2023-09-25 00:42:17 +0800

    添加数据

--- src/app/factory/factory-map/factory-map.component.ts
+++ src/app/factory/factory-map/factory-map.component.ts
@@ -66,6 +66,7 @@ export const areas: {
     number: 33,
     x: 282.5018310546875,
     y: 126.58538055419922,
+    imgSrc: '/static/9月24日新增/IEDS转子/145KW转子.JPG'
   },
   {
     area: '生产二区圆铜线',
@@ -88,6 +89,10 @@ export const areas: {
     number: 4,
     x: 306.38519287109375,
     y: 173.53599548339844,
+    videoSrc: [
+      '/static/9月24日新增/小电轴定子/安装导油罩.MOV',
+      '/static/9月24日新增/小电轴定子/导油罩翻转.MOV',
+    ]
   },
   {
     area: '生产二区扁铜线',
@@ -175,6 +180,13 @@ export const areas: {
     number: 19,
     x: 84.57540893554688,
     y: 243.37046813964844,
+    imgSrc: '/static/9月24日新增/MEB转子二期/BEV3转子.JPG',
+    videoSrc: [
+      '/static/9月24日新增/MEB转子二期/压环工位.mp4',
+      '/static/9月24日新增/MEB转子二期/感应加热工位.mp4',
+      '/static/9月24日新增/MEB转子二期/机器人区域1.mp4',
+      '/static/9月24日新增/MEB转子二期/车外圆工位.mp4',
+    ]
   },
   {
     area: '生产三区',
@@ -263,49 +275,49 @@ export const areas: {
   },
 ];
 
-const area2 = [
-  { area: '生产二区圆铜线', line: 'MEB转子', number: 19, x: 174, y: 262 },
-  { area: '生产二区扁铜线', line: 'IEDS转子', number: 33, x: 408, y: 162 },
-  { area: '生产二区圆铜线', line: 'MEB定子', number: 6, x: 302, y: 215 },
-  { area: '生产二区圆铜线', line: 'MEB电枢线', number: 37, x: 350, y: 246 },
-  { area: '生产二区扁铜线', line: '小电轴定子', number: 4, x: 457, y: 173 },
-  { area: '生产二区扁铜线', line: '小电轴电枢', number: 43, x: 477, y: 188 },
-  { area: '生产一区滴漆', line: '滴漆（MEB）', number: 0, x: 595, y: 84 },
-  { area: '生产一区滴漆', line: '滴漆（小电轴）', number: 12, x: 679, y: 109 },
-  { area: '生产一区', line: 'EDU电枢', number: 23, x: 486, y: 108 },
-  { area: '生产一区', line: 'EV2电枢', number: 29, x: 620, y: 122 },
-  { area: '生产一区滴漆', line: '滴漆（EV&EDU）', number: 8, x: 623, y: 85 },
-  { area: '生产一区', line: 'EDU总装', number: 46, x: 572, y: 152 },
-  { area: '生产一区', line: 'EV2总装', number: 41, x: 521, y: 87 },
-  { area: '生产三区', line: 'BEV3漆前电枢', number: 18, x: 497, y: 357 },
-  { area: '生产二区圆铜线', line: 'MEB转子二期', number: 19, x: 178, y: 243 },
-  { area: '生产三区', line: 'VOLVO总装线', number: 25, x: 864, y: 189 },
-  { area: '生产三区', line: 'VOLVO漆后定子线', number: 7, x: 753, y: 180 },
-  {
-    area: '生产一区滴漆',
-    line: '绝缘车间（VOLVO）',
-    number: 20,
-    x: 787,
-    y: 156,
-  },
-  {
-    area: '生产一区滴漆',
-    line: 'MEB及小电轴滴漆扩能产线',
-    number: 0,
-    x: 733,
-    y: 132,
-  },
-  { area: '生产二区扁铜线', line: 'C轴电枢线', number: 27, x: 436, y: 190 },
-  { area: '生产三区', line: 'BEV3漆后电枢', number: 6, x: 610, y: 430 },
-  {
-    area: '生产二区扁铜线',
-    line: '注塑转子二号线',
-    number: 29,
-    x: 409,
-    y: 374,
-  },
-  { area: '生产三区', line: 'VOLVO定子线', number: 15, x: 802, y: 194 },
-];
+// const area2 = [
+//   { area: '生产二区圆铜线', line: 'MEB转子', number: 19, x: 174, y: 262 },
+//   { area: '生产二区扁铜线', line: 'IEDS转子', number: 33, x: 408, y: 162 },
+//   { area: '生产二区圆铜线', line: 'MEB定子', number: 6, x: 302, y: 215 },
+//   { area: '生产二区圆铜线', line: 'MEB电枢线', number: 37, x: 350, y: 246 },
+//   { area: '生产二区扁铜线', line: '小电轴定子', number: 4, x: 457, y: 173 },
+//   { area: '生产二区扁铜线', line: '小电轴电枢', number: 43, x: 477, y: 188 },
+//   { area: '生产一区滴漆', line: '滴漆（MEB）', number: 0, x: 595, y: 84 },
+//   { area: '生产一区滴漆', line: '滴漆（小电轴）', number: 12, x: 679, y: 109 },
+//   { area: '生产一区', line: 'EDU电枢', number: 23, x: 486, y: 108 },
+//   { area: '生产一区', line: 'EV2电枢', number: 29, x: 620, y: 122 },
+//   { area: '生产一区滴漆', line: '滴漆（EV&EDU）', number: 8, x: 623, y: 85 },
+//   { area: '生产一区', line: 'EDU总装', number: 46, x: 572, y: 152 },
+//   { area: '生产一区', line: 'EV2总装', number: 41, x: 521, y: 87 },
+//   { area: '生产三区', line: 'BEV3漆前电枢', number: 18, x: 497, y: 357 },
+//   { area: '生产二区圆铜线', line: 'MEB转子二期', number: 19, x: 178, y: 243 },
+//   { area: '生产三区', line: 'VOLVO总装线', number: 25, x: 864, y: 189 },
+//   { area: '生产三区', line: 'VOLVO漆后定子线', number: 7, x: 753, y: 180 },
+//   {
+//     area: '生产一区滴漆',
+//     line: '绝缘车间（VOLVO）',
+//     number: 20,
+//     x: 787,
+//     y: 156,
+//   },
+//   {
+//     area: '生产一区滴漆',
+//     line: 'MEB及小电轴滴漆扩能产线',
+//     number: 0,
+//     x: 733,
+//     y: 132,
+//   },
+//   { area: '生产二区扁铜线', line: 'C轴电枢线', number: 27, x: 436, y: 190 },
+//   { area: '生产三区', line: 'BEV3漆后电枢', number: 6, x: 610, y: 430 },
+//   {
+//     area: '生产二区扁铜线',
+//     line: '注塑转子二号线',
+//     number: 29,
+//     x: 409,
+//     y: 374,
+//   },
+//   { area: '生产三区', line: 'VOLVO定子线', number: 15, x: 802, y: 194 },
+// ];
 
 @Component({
   selector: 'app-factory-map',
@@ -339,15 +351,15 @@ export class FactoryMapComponent implements AfterViewInit {
     this.currentChange.emit(this.current);
   }
 
-  move(ev: { x: number; y: number }, item: (typeof areas)[number]) {
-    this.current = item;
-    this.isShow = true;
-    const tmp = area2.find((a) => a.line === item.line)!;
-    tmp.x = ev.x;
-    tmp.y = ev.y;
-  }
+  // move(ev: { x: number; y: number }, item: (typeof areas)[number]) {
+  //   this.current = item;
+  //   this.isShow = true;
+  //   const tmp = area2.find((a) => a.line === item.line)!;
+  //   tmp.x = ev.x;
+  //   tmp.y = ev.y;
+  // }
 
-  log() {
-    console.log(area2);
-  }
+  // log() {
+  //   console.log(area2);
+  // }
 }
